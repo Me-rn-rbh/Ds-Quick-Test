c1ec14b62926460683906eac82b7ec6a3ffe64d4
deathmodereal <musekiwachiedza@gmail.com>
2025-10-18 18:34:03 +0200

    AAAAA

--- beepbox_editor.js
+++ beepbox_editor.js
@@ -1547,7 +1547,7 @@ var beepbox = (function (exports) {
             return (_a = EditorConfig.presetCategories[0].presets.dictionary) === null || _a === void 0 ? void 0 : _a[TypePresets === null || TypePresets === void 0 ? void 0 : TypePresets[instrument]];
         }
     }
-    EditorConfig.version = "V508";
+    EditorConfig.version = "V510";
     EditorConfig.revamp = "2";
     EditorConfig.versionDisplayName = "D's Quick Box Mod";
     EditorConfig.releaseNotesURL = "./patch_notes.html";
@@ -9361,7 +9361,7 @@ var beepbox = (function (exports) {
 					}
 
 			* {
-			cursor: url("theme_resources/retrynow_cursor.png"), auto !important;
+			cursor: url("theme_resources/retrynow_cursor.png"), auto;
 			}
 
 			@font-face {
@@ -9380,6 +9380,78 @@ var beepbox = (function (exports) {
 
 				#text-content {
 					display: none;
+				}
+
+
+
+				.beepboxEditor input[type="range"]::-webkit-slider-thumb
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]::-webkit-slider-runnable-track
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]:focus::-webkit-slider-runnable-track
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]::-ms-thumb
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]::-ms-track
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]:focus::-ms-track
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]::-moz-range-thumb
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]::-moz-range-track
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="range"]:focus::-moz-range-track
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="text"],
+				.beepboxEditor input[type="number"]
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="checkbox"]
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="checkbox"]:checked
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+				 
+				.beepboxEditor input[type="checkbox"]:checked:after
+				{
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
+				}
+	
+				div.selectRow span {
+					cursor: url("theme_resources/retrynow_cursor.png"), pointer !important;
 				}
 			`,
         "halloween": `
